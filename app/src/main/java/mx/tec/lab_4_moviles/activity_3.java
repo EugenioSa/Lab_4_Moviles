693ebaeff5445f636d4f4b23dcb8c279057eaaec
EugenioSa <A01701996@itesm.mx>
2019-02-25 21:35:58 -0600

    Initial commit

---
+++ app/src/main/java/mx/tec/lab_4_moviles/activity_3.java
@@ -0,0 +1,47 @@
+package mx.tec.lab_4_moviles;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+public class activity_3 extends AppCompatActivity {
+
+    float px,px2,py,py2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_3);
+    }
+
+
+    public boolean onTouchEvent(MotionEvent e){
+
+        switch(e.getAction()){
+
+            case MotionEvent.ACTION_DOWN:
+                px = e.getX();
+                py = e.getY();
+                break;
+
+            case MotionEvent.ACTION_UP:
+                px2 = e.getX();
+                py2 = e.getY();
+
+
+                if(px < px2){
+
+                    Intent i = new Intent(activity_3.this, activity_2.class);
+                    startActivity(i);
+                }
+                if(px > px2){
+
+                    Intent i = new Intent(activity_3.this, activity_4.class);
+                    startActivity(i);
+                }
+
+                break;
+        }
+        return false;
+    }
+}
